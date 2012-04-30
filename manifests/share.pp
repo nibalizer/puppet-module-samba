fdd3c4c5462b5818267edf1821e810574066c733
Spencer Krum <nibz@cat.pdx.edu>
2012-04-29 22:21:48 -0700

    samba::global now only includes user specefied options

--- manifests/share.pp
+++ manifests/share.pp
@@ -1,12 +1,25 @@
-define samba::dfsroot(
-  $msdfs_root           = 'yes',
-  $msdfs_proxy          = '',
-  $comment              = 'default comment',
-  $path                 = '/var/samba/dfsname',
-  $public               = 'no',
-  $smb_conf_filename    = '/etc/samba/smb.conf',
-  $ensure               = present,
-  $priority             = '20'
+define samba::share(
+  $msdfs_root,
+  $msdfs_proxy,
+  $comment,
+  $path,
+  $writeable,
+  $force_create_mode,
+  $force_directory_mode,
+  $force_group,
+  $public,
+  $create_mask,
+  $directory_mask,
+  $inherit_acls,
+  $inherit_permissions,
+  $acl_check permissions,
+  $vfs_objects,
+  $nfs4_mode,
+  $nfs4_acedup,
+  $nfs4_chown,
+  $smb_conf_filename      = '/etc/samba/smb.conf',
+  $ensure                 = present,
+  $priority               = '20'
 ) {
   include samba
 
@@ -19,8 +32,9 @@ define samba::dfsroot(
   concat::fragment { $name:
     ensure  => $ensure,
     target  => $smb_conf_filename,
-    content => template('samba/dfsroot.erb'),
+    content => template('samba/share.erb'),
     order   => $priority,
   }
 
 }
+
